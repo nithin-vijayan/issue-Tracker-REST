9ff6bb0b5c34391a5b046c76d72b75c7d44320c8
Nithin K <vijayan@apple.com>
2018-05-10 01:57:38 +0530

    Added model class for issues

--- issuetracker/models.py
+++ issuetracker/models.py
@@ -1,6 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Issue(models.Model):
+	TICKET_STATUS = (
+		('O', 'OPEN'),
+		('C', 'CLOSE'),
+		)
+	title = models.CharField(max_length=50)
+	description = models.CharField(max_length=200)
+	assigned_to = models.ForeignKey(User, related_name='assigned_to')
+	created_by = models.ForeignKey(User, related_name='created_by')
+	status = models.CharField(max_length=1, choices=TICKET_STATUS)
