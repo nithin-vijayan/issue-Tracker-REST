c709c27d67a12a0e8066299fb97e3610c3e3d2e8
Nithin K <nithinkvijayan@icloud.com>
2018-05-10 00:25:55 +0530

    Added apps to installed apps

--- issueTrackerREST/settings.py
+++ issueTrackerREST/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usermanager',
+    'issuetracker',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
