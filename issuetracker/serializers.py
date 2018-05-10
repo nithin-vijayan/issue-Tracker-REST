035a70cac36921555dd088a83708a3bac5472efd
Nithin K <nithinkvijayan@icloud.com>
2018-05-10 22:56:45 +0530

    Changed createdby to readonly field

--- issuetracker/serializers.py
+++ issuetracker/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Issue
 
 class IssueSerializer(serializers.ModelSerializer):
+    created_by = serializers.ReadOnlyField(source='created_by.username')
     class Meta:
         model = Issue
         fields = '__all__'
