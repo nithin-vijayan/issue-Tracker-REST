f409e48122aa46109cadcd37ea961bbd3c9b5068
Nithin K <nithinkvijayan@icloud.com>
2018-05-13 08:19:12 +0530

    set permission class to endpoints for object level permission

--- issuetracker/views.py
+++ issuetracker/views.py
@@ -1,6 +1,8 @@
 from .models import Issue
 from .serializers import IssueSerializer
 from rest_framework import generics
+from .permissions import IsOwnerOrReadOnly
+from rest_framework import permissions
 
 class IssueList(generics.ListCreateAPIView):
     queryset = Issue.objects.all()
@@ -9,6 +11,7 @@ class IssueList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+class IssueDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
+    permission_classes = (IsOwnerOrReadOnly, permissions.IsAuthenticated)
