64f9e0adf3d4575e81816406da87af6246c52c92
Nithin K <vijayan@apple.com>
2018-05-13 08:10:19 +0530

    Added field tracker for assigned_to for tracking assignee changes

--- issuetracker/models.py
+++ issuetracker/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from model_utils import FieldTracker
+
 
 class Issue(models.Model):
 	TICKET_STATUS = (
@@ -11,3 +13,7 @@ class Issue(models.Model):
 	assigned_to = models.ForeignKey(User, related_name='assigned_to', to_field='username')
 	created_by = models.ForeignKey(User, related_name='created_by', to_field='username')
 	status = models.CharField(max_length=6, choices=TICKET_STATUS)
+
+	#For tracking changes in assigned_to field while changing ticket assignee
+	assign_tracker = FieldTracker(fields=['assigned_to'])
+
